9fdc9811fc41f7e496d93a945ea90ba10fc4aff2
CammiePone <inazuma261@gmail.com>
2020-12-06 20:03:25 -0500

    Initial commit

---
+++ src/main/java/com/camscorner/icarus/Icarus.java
@@ -0,0 +1,12 @@
+package com.camscorner.icarus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Icarus implements ModInitializer
+{
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/com/camscorner/icarus/client/IcarusClient.java
@@ -0,0 +1,15 @@
+package com.camscorner.icarus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class IcarusClient implements ClientModInitializer
+{
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
