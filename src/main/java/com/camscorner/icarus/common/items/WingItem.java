86a567090b016cb234492ae3a4463291a81e8d6b
CammiePone <inazuma261@gmail.com>
2021-08-30 12:09:37 -0400

    added equip sound and repair material

--- src/main/java/com/camscorner/icarus/common/items/WingItem.java
+++ src/main/java/com/camscorner/icarus/common/items/WingItem.java
@@ -14,12 +14,16 @@ import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.Tag;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.caelus.api.CaelusApi;
 
 import java.util.UUID;
@@ -102,6 +106,17 @@ public class WingItem extends TrinketItem {
 	@Override
 	public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
 		return this.attributeModifiers;
+	}
+
+	@Override
+	public boolean canRepair(ItemStack stack, ItemStack ingredient) {
+		return ingredient.isOf(Items.PHANTOM_MEMBRANE);
+	}
+
+	@Nullable
+	@Override
+	public SoundEvent getEquipSound() {
+		return SoundEvents.ITEM_ARMOR_EQUIP_ELYTRA;
 	}
 
 	public DyeColor getPrimaryColour() {
