fbb5291076a0497657acee63b4fb1f1a02f385be
Up <upcraftedlp@gmail.com>
2025-01-31 17:14:07 +0100

    fix deprecation warning

--- Common/src/main/java/dev/cammiescorner/icarus/client/IcarusClientConfig.java
+++ Common/src/main/java/dev/cammiescorner/icarus/client/IcarusClientConfig.java
@@ -3,11 +3,10 @@ package dev.cammiescorner.icarus.client;
 
 import com.teamresourceful.resourcefulconfig.api.annotations.Category;
 import com.teamresourceful.resourcefulconfig.api.annotations.ConfigEntry;
-import com.teamresourceful.resourcefulconfig.api.types.options.EntryType;
 
 @Category("client")
 public final class IcarusClientConfig {
 
-    @ConfigEntry(id = "can_loop_de_loop", type = EntryType.BOOLEAN, translation = "config.icarus.can_loop_de_loop_client")
+    @ConfigEntry(id = "can_loop_de_loop", translation = "config.icarus.can_loop_de_loop_client")
     public static boolean canLoopDeLoop = true;
 }
